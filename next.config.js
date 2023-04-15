814e4ebbab261a5357dfff37b4720953f84bc8e9
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-04-15 22:01:31 +0330

    bloody next XD

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+	experimental: {
+		appDir: true,
+	},
+	env: {
+		API_KEY: process.env.API_KEY,
+	},
 }
 
 module.exports = nextConfig
